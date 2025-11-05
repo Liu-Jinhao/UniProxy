26fcedd1088f40d0228dae3d3320f218840a8c3a
Liu-Jinhao <92904855+Liu-Jinhao@users.noreply.github.com>
2025-11-05 19:30:12 +0800

    Delete quic-tun/redirect.sh

--- quic-tun/redirect.sh
+++
@@ -1,3 +0,0 @@
-ip rule add fwmark 1 table 100
-ip route add local 0.0.0.0/0 dev lo table 100
-iptables -t mangle -A PREROUTING --in-interface r1-eth0 -p tcp -d 10.0.7.0/24 -j TPROXY --on-port 6500 --on-ip 127.0.0.1 --tproxy-mark 1/1
