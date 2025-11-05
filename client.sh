b1825c72aa858b6754410245b73ef94cf31af021
Liu-Jinhao <ljh2019@mail.ustc.edu.cn>
2025-11-05 17:41:16 +0800

    Initial commit

---
+++ client.sh
@@ -0,0 +1,3 @@
+cd /home/a/quic-tun/client/cmd
+../../redirect.sh
+./cmd --listen-on tcp:127.0.0.1:6500 --server-endpoint 10.0.6.2:7500
---
+++ server.sh
@@ -0,0 +1,2 @@
+cd /home/a/quic-tun/server/cmd
+./cmd --listen-on 10.0.6.2:7500
